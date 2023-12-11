23d65c0a14d6322f07b12b00a9d5997d54783dc6
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-12-11 12:15:55 +0100

    wip

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-br from-rose-100 to-teal-100">
+    <nav class="container mx-auto px-3 h-24 flex items-center mb-10">
+      <NuxtLink to="/">
+        <h1 class="text-5xl">Startup</h1>
+      </NuxtLink>
+    </nav>
+    <div class="container mx-auto px-3">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const query = gql`
+  query Page {
+    page(where: { slug: "accueil" }) {
+      createdAt
+      id
+      publishedAt
+      slug
+      titre
+      updatedAt
+      texte {
+        html
+      }
+    }
+  }
+`;
+
+const contenuAccueil = ref();
+const { data } = await useAsyncQuery(query);
+console.log(data.value);
+contenuAccueil.value = data.value.page;
+</script>
+
+<template>
+  <h2 class="text-2xl">
+    {{ contenuAccueil.titre }}
+  </h2>
+
+  <div v-html="contenuAccueil.texte.html"></div>
+</template>

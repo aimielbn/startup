68c1dbd096c3f34debb7ca17460b1a8903979d8b
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-12-15 11:11:21 +0100

    wip

---
+++ pages/societe.vue
@@ -0,0 +1,30 @@
+<script setup>
+const query = gql`
+  query Page {
+    page(where: { slug: "societe" }) {
+      createdAt
+      id
+      publishedAt
+      slug
+      titre
+      updatedAt
+      texte {
+        html
+      }
+    }
+  }
+`;
+
+const contenuSociete = ref();
+const { data } = await useAsyncQuery(query);
+console.log(data.value);
+contenuSociete.value = data.value.page;
+</script>
+
+<template>
+  <h2 class="text-2xl">
+    {{ contenuSociete.titre }}
+  </h2>
+
+  <div v-html="contenuSociete.texte.html"></div>
+</template>
